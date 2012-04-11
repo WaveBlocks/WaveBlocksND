15c9bba1e09b7f6976ac17085d2ea9d03df5252a
Raoul Bourquin <raoulb@bluewin.ch>
2012-04-11 17:29:11 +0200

    Improve kinetic energy computation of wavepackets

--- src/WaveBlocksND/ObservablesHAWP.py
+++ src/WaveBlocksND/ObservablesHAWP.py
@@ -1,7 +1,7 @@
 """The WaveBlocks Project
 
 Compute some observables like kinetic and potential energy
-of a Hagedorn wavepacket.
+of Hagedorn wavepackets.
 
 @author: R. Bourquin
 @copyright: Copyright (C) 2012 R. Bourquin
@@ -18,11 +18,17 @@ __all__ = ["ObservablesHAWP"]
 
 
 class ObservablesHAWP(Observables):
-    r"""This class implements observable computation for Hagedorn wavepackets.
+    r"""This class implements observable computation for Hagedorn wavepackets :math:`\Psi`.
     """
 
     def __init__(self, quadrature=None):
-        r"""
+        r"""Initialize a new :py:class:`ObservablesHAWP` instance for observable computation
+        of Hagedorn wavepackets.
+
+        :param quadrature: A quadrature for computing the integrals. Quadrature is only used for
+                           the computation of the potential energy :math:`\langle\Psi|V(x)|\Psi\rangle`
+                           but not for the kinetic energy.
+        :type quadrature: A :py:class:`Quadrature` subclass instance.
         """
         # A quadrature to compute the integrals
         if quadrature is not None:
@@ -30,60 +36,62 @@ class ObservablesHAWP(Observables):
 
 
     def set_quadrature(self, quadrature):
-        r"""
+        r"""Set the quadrature.
+
+        :param quadrature: A quadrature for computing the integrals. Quadrature is only used for
+                           the computation of the potential energy :math:`\langle\Psi|V(x)|\Psi\rangle`
+                           but not for the kinetic energy.
+        :type quadrature: A :py:class:`Quadrature` subclass instance.
         """
         self._quadrature = quadrature
 
 
     def apply_gradient(self, wavepacket, component):
-        r"""Compute the effect of the operator :math:`-i \varepsilon^2 \nabla_x` on the basis
-        functions :math:`\phi` of a component :math:`\Phi_i` of the Hagedorn wavepacket :math:`\Psi`.
+        r"""Compute the effect of the gradient operator :math:`-i \varepsilon^2 \nabla_x` on the basis
+        functions :math:`\phi(x)` of a component :math:`\Phi_i` of the Hagedorn wavepacket :math:`\Psi`.
 
-        :param wavepacket:
-        :param component:
+        :param wavepacket: The wavepacket :math:`\Psi` containing :math:`\Phi_i`.
+        :type wavepacket: A :py:class:`HagedornWavepacketBase` subclass instance.
+        :param component: The index :math:`i` of the component :math:`\Phi_i`.
+        :type component: int
         :return: Extended basis shape :math:`\mathcal{\dot{K}}` and new coefficients :math:`c^\prime`.
         """
-        # TODO: Maybe move this method into the HAWP class?
+        # TODO: Consider moving this method into the HAWP class?
         D = wavepacket.get_dimension()
-        K = wavepacket.get_basis_shape(component=component)
-
         eps = wavepacket.get_eps()
         q, p, Q, P, S = wavepacket.get_parameters(component=component)
         Pbar = conjugate(P)
         coeffs = wavepacket.get_coefficients(component=component)
 
+        # Prepare storage for new coefficients
+        K = wavepacket.get_basis_shape(component=component)
         size = K.get_basis_size(extended=True)
         cnew = zeros((size,D), dtype=complexfloating)
 
         # We implement the less efficient gather type stencil here
-        I = K.get_node_iterator(extended=True)
-
-        for i in I:
-            # Current phi_i coefficient
+        for i in K.get_node_iterator(extended=True):
+            # Central phi_i coefficient
             if i in K:
                 ccur = coeffs[K[i]]
             else:
                 ccur = 0.0
 
-            # Backward neighbours phi_i - <e>
+            # Backward neighbours phi_{i - e_d}
             cbw = zeros((D,1), dtype=complexfloating)
             nbw = K.get_neighbours(i, selection="backward", extended=False)
 
             for d, nb in nbw:
-                j = K[nb]
-                cbw[d] = coeffs[j] * sqrt(i[d])
+                cbw[d] = coeffs[K[nb]] * sqrt(i[d])
 
-            # Forward neighbours phi_i + <e>
-            nfw = K.get_neighbours(i, selection="forward", extended=False)
+            # Forward neighbours phi_{i + e_d}
             cfw = zeros((D,1), dtype=complexfloating)
+            nfw = K.get_neighbours(i, selection="forward", extended=False)
 
             for d, nb in nfw:
-                j = K[nb]
-                cfw[d] = coeffs[j] * sqrt(i[d] + 1.0)
+                cfw[d] = coeffs[K[nb]] * sqrt(i[d] + 1.0)
 
             # Compute parts and assemble
-            res = sqrt(eps**2/2.0) * (dot(P, cfw) + dot(Pbar, cbw)) + p * ccur
-            cnew[K[i],:] = squeeze(res)
+            cnew[K[i],:] = squeeze(sqrt(eps**2/2.0) * (dot(P, cfw) + dot(Pbar, cbw)) + p * ccur)
 
         return (K, cnew)
 
@@ -94,35 +102,51 @@ class ObservablesHAWP(Observables):
 
         :param wavepacket: The wavepacket :math:`\Psi` of which we compute the kinetic energy.
         :type wavepacket: A :py:class:`HagedornWavepacketBase` subclass instance.
-        :param component:
+        :param component: The index :math:`i` of the component :math:`\Phi_i` whose
+                          kinetic energy we want to compute. If set to ``None`` the
+                          computation is performed for all :math:`N` components.
+        :type component: Integer or ``None``.
         :param summed: Whether to sum up the kinetic energies :math:`E_i` of the individual
                        components :math:`\Phi_i`. Default is `False`.
+        :type summed: Boolean
         :return: A list with the kinetic energies of the individual components or the
                  overall kinetic energy of the wavepacket. (Depending on the optional arguments.)
         """
-        N = wavepacket.get_number_components()
+        if component is None:
+            N = wavepacket.get_number_components()
+            components = xrange(N)
+        else:
+            components = [component]
 
         ekin = []
 
-        for n in xrange(N):
+        for n in components:
             Kprime, cnew = self.apply_gradient(wavepacket, n)
             ekin.append(0.5 * sum(sum(conjugate(cnew)*cnew, axis=1), axis=0))
 
         if summed is True:
             ekin = sum(ekin)
+        elif component is not None:
+            # Do not return a list for specific single components
+            ekin = ekin[0]
 
         return ekin
 
 
-    def potential_energy(self, wavepacket, potential, summed=False):
+    def potential_energy(self, wavepacket, potential, component=None, summed=False):
         r"""Calculate the potential energy :math:`E_{\text{pot}} := \langle\Psi|V|\Psi\rangle`
         of the different components :math:`\Phi_i` of the wavepacket :math:`\Psi`.
 
         :param wavepacket: The wavepacket :math:`\Psi` of which we compute the potential energy.
         :type wavepacket: A :py:class:`HagedornWavepacketBase` subclass instance.
         :param potential: The potential :math:`V(x)`.
+        :param component: The index :math:`i` of the component :math:`\Phi_i` whose
+                          potential energy we want to compute. If set to ``None`` the
+                          computation is performed for all :math:`N` components.
+        :type component: Integer or ``None``.
         :param summed: Whether to sum up the potential energies :math:`E_i` of the individual
                        components :math:`\Phi_i`. Default is `False`.
+        :type summed: Boolean
         :return: A list with the potential energies of the individual components or the
                  overall potential energy of the wavepacket. (Depending on the optional arguments.)
         """
