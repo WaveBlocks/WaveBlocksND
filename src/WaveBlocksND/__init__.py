5ba964edd6d157517dafa068ab2f7f43e4770c41
Raoul Bourquin <raoulb@student.ethz.ch>
2012-06-01 16:34:25 +0200

    Added a missing class to the WaveBlocksND module

--- src/WaveBlocksND/__init__.py
+++ src/WaveBlocksND/__init__.py
@@ -109,6 +109,8 @@ from ParameterProvider import ParameterProvider
 
 from TimeManager import TimeManager
 
+from Initializer import Initializer
+
 from SimulationLoop import SimulationLoop
 from SimulationLoopFourier import SimulationLoopFourier
 from SimulationLoopHagedorn import SimulationLoopHagedorn
