1f1aed8d522ddb008a3fcc19b6f3cb9ba3ede127
Raoul Bourquin <raoulb@student.ethz.ch>
2012-06-26 17:05:34 +0200

    A harmonic ring-like potential

---
+++ examples/harmonic_oscillators/harmonic_ring_2D_p.py
@@ -0,0 +1,65 @@
+algorithm = "hagedorn"
+
+T = 10
+dt = 0.01
+
+dimension = 2
+ncomponents = 1
+
+eps = 0.1
+
+# A ring like potential
+potential = {}
+potential["potential"] = [["(sqrt(x**2+y**2) -R)**2"]]
+potential["variables"] = ["x", "y"]
+
+R = 4
+
+# The parameter set of the initial wavepacket
+Q = [[1.0, 0.0],
+     [0.0, 1.0]]
+
+P = [[1.0j, 0.0 ],
+     [0.0,  1.0j]]
+
+q = [[ 4.2],
+     [ 0.0]]
+
+p = [[0.0],
+     [0.2]]
+
+S = [[0.0]]
+
+# What it takes to specify a wavepacket!
+wp0 = {
+    "type" : "HagedornWavepacket",
+    "dimension" : dimension,
+    "ncomponents": 1,
+    "eps" : eps,
+    "Pi" : [q,p,Q,P,S],
+    "basis_shapes" : [{
+        "type" : "HyperbolicCutShape",
+        "K" : 16,
+        "dimension" : dimension
+    }],
+    "coefficients" : [[ ((0,0), 1.0) ]],
+    "quadrature" : {
+        "type" : "HomogeneousQuadrature",
+	'qr': {
+            'type': 'TensorProductQR',
+            'dimension': dimension,
+            'qr_rules': [{'dimension': 1, 'order': 20, 'type': 'GaussHermiteQR'},
+                         {'dimension': 1, 'order': 20, 'type': 'GaussHermiteQR'}],
+        }
+    }
+}
+
+# Which wavepackets are initial values
+initvals = [ wp0 ]
+
+leading_component = 0
+
+# How often do we write data to disk
+write_nth = 5
+
+matrix_exponential = "pade"
