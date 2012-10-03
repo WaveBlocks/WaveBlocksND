c12d6ee83cbce2d0145ce1ff1d1bb8807e938ebb
Raoul Bourquin <raoulb@student.ethz.ch>
2012-10-03 10:14:47 +0200

    Adding a simple install script

---
+++ src/sh/install_wb.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+
+SRCPATH="$HOME/WaveBlocksND/"
+
+if [ $# -lt 1 ]
+    then
+    IPATH="$HOME/wbi";
+    echo "Need an installation path, None given, using default";
+else
+    IPATH=$1;
+fi
+
+echo "Install WB to location $IPATH";
+
+# Create dir if necessary
+if [ ! -d $IPATH ]
+    then
+    mkdir -p $IPATH
+fi
+
+# Copy scripts
+cp $SRCPATH/src/scripts/* $IPATH/
+cp $SRCPATH/src/plotters/* $IPATH/
