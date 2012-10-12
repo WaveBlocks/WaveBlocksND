bc31a78f11f16cad686f57a34ce9fe958f8d6554
Raoul Bourquin <raoulb@student.ethz.ch>
2012-10-12 18:26:56 +0200

    Simulation setup templates

---
+++ examples/templates/template_1D_p.py
@@ -0,0 +1,53 @@
+algorithm = "hagedorn"
+propagator = "semiclassical"
+splitting_method = "Y4"
+
+T =
+dt =
+
+dimension = 1
+ncomponents = 1
+
+eps =
+
+potential =
+
+# The parameter set of the initial wavepacket
+Q = [[1.0]]
+P = [[1.0j]]
+q = [[1.0]]
+p = [[0.0]]
+S = [[0.0]]
+
+leading_component =
+
+# How often do we write data to disk
+write_nth = 1
+
+matrix_exponential = "pade"
+
+# What it takes to specify a wavepacket!
+wp0 = {
+    "type" : "HagedornWavepacket",
+    "dimension" : 1,
+    "ncomponents": 1,
+    "eps" : eps,
+    "Pi" : [q,p,Q,P,S],
+    "basis_shapes" : [{
+        "type" : "HyperbolicCutShape",
+        "K" : 10,
+        "dimension" : 1
+    }],
+    "coefficients" : [[ ((0,), 1.0) ]],
+    "quadrature" : {
+        "type" : "HomogeneousQuadrature",
+	'qr': {
+            'type': 'TensorProductQR',
+            'dimension': 1,
+            'qr_rules': [{'dimension': 1, 'order': 14, 'type': 'GaussHermiteQR'}]
+        }
+    }
+}
+
+# Which wavepackets are initial values
+initvals = [ wp0 ]
---
+++ examples/templates/template_2D_p.py
@@ -0,0 +1,62 @@
+algorithm = "hagedorn"
+propagator = "semiclassical"
+splitting_method = "Y4"
+
+T =
+dt =
+
+dimension = 2
+ncomponents = 1
+
+eps =
+
+potential =
+
+# The parameter set of the initial wavepacket
+Q = [[1.0, 0.0],
+     [0.0, 1.0]]
+
+P = [[1.0j, 0.0 ],
+     [0.0,  1.0j]]
+
+q = [[-1.0],
+     [ 0.0]]
+
+p = [[0.0],
+     [0.0]]
+
+S = [[0.0]]
+
+leading_component = 0
+
+# How often do we write data to disk
+write_nth = 1
+
+matrix_exponential = "pade"
+
+# What it takes to specify a wavepacket!
+wp0 = {
+    "type" : "HagedornWavepacket",
+    "dimension" : 2,
+    "ncomponents": 1,
+    "eps" : eps,
+    "Pi" : [q,p,Q,P,S],
+    "basis_shapes" : [{
+        "type" : "HyperbolicCutShape",
+        "K" : 8,
+        "dimension" : 2
+    }],
+    "coefficients" : [[ ((0,0), 1.0) ]],
+    "quadrature" : {
+        "type" : "HomogeneousQuadrature",
+	'qr': {
+            'type': 'TensorProductQR',
+            'dimension': 2,
+            'qr_rules': [{'dimension': 1, 'order': 24, 'type': 'GaussHermiteQR'},
+                         {'dimension': 1, 'order': 24, 'type': 'GaussHermiteQR'}],
+        }
+    }
+}
+
+# Which wavepackets are initial values
+initvals = [ wp0 ]
