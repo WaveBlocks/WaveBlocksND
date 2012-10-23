9ccb99144d5c725a44754b9d467ca34d967626ff
Raoul Bourquin <raoulb@student.ethz.ch>
2012-10-23 18:42:42 +0200

    Example of a single avoided crossing

---
+++ examples/single_avoided_crossing/single_crossing_1D_p.py
@@ -0,0 +1,59 @@
+algorithm = "hagedorn"
+
+T = 10
+dt = 0.01
+
+dimension = 1
+ncomponents = 2
+
+eps = 0.2
+delta = eps
+
+potential = "delta_gap"
+
+leading_component = 0
+
+# The parameter set of the initial wavepacket
+Q = [[1.0-5.0j]]
+P = [[1.0j]]
+q = [[-5.0]]
+p = [[ 1.0]]
+S = [[0.0]]
+
+# What it takes to specify a wavepacket!
+wp0 = {
+    "type" : "HagedornWavepacket",
+    "dimension" : dimension,
+    "ncomponents": ncomponents,
+    "eps" : eps,
+    "Pi" : [q,p,Q,P,S],
+    "basis_shapes" : [{
+            "type" : "HyperbolicCutShape",
+            "K" : 64,
+            "dimension" : 1
+            },
+            {
+            "type" : "HyperbolicCutShape",
+            "K" : 64,
+            "dimension" : 1
+            }],
+    "coefficients" : [[ ((0,), 1.0) ],
+                      [ ((0,), 0.0) ]
+                      ],
+    "quadrature" : {
+        "type" : "HomogeneousQuadrature",
+	'qr': {
+            'type': 'TensorProductQR',
+            'dimension': 1,
+            'qr_rules': [{'dimension': 1, 'order': 68, 'type': 'GaussHermiteQR'}]
+        }
+    }
+}
+
+# Which wavepackets are initial values
+initvals = [ wp0 ]
+
+# How often do we write data to disk
+write_nth = 2
+
+matrix_exponential = "pade"
