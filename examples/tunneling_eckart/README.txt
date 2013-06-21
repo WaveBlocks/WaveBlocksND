16799a19d0d83c0922315ff69514c25afbc6e9bd
Raoul Bourquin <raoulb@student.ethz.ch>
2013-06-21 13:05:10 +0200

    Update tunneling examples

---
+++ examples/tunneling_eckart/README.txt
@@ -0,0 +1,28 @@
+The Eckart tunneling examples are taken from the paper:
+
+@article{GHJ_tunneling_spawning,
+        author = {Vasile Gradinaru and George A. Hagedorn and Alain Joye},
+	title = {Tunneling dynamics and spawning with adaptive semiclassical wave packets},
+	journal = {The Journal of Chemical Physics},
+	volume = {132},
+	number = {18},
+	publisher = {AIP},
+	year = {2010},
+	doi = {10.1063/1.3429607}
+}
+
+Note that there is a misprint in the initial values for Q(0) and P(0).
+The values should be Q(0) = 1 / sqrt(2 * 0.04) and P(0) = i * sqrt(2 * 0.04).
+
+The data for the model potential come from:
+
+@article {PA_tddvr,
+	 author = {Puzari, Panchanan and Adhikari, Satrajit},
+	 title = {Semi-classical formulation of time-dependent discrete variable representation method},
+	 journal = {International Journal of Quantum Chemistry},
+	 volume = {98},
+	 number = {5},
+	 publisher = {Wiley Subscription Services, Inc., A Wiley Company},
+	 year = {2004},
+	 doi = {10.1002/qua.20095}
+}
---
+++ examples/tunneling_eckart/eckart_phi0_p.py
@@ -0,0 +1,60 @@
+algorithm = "hagedorn"
+propagator = "semiclassical"
+splitting_method = "Y4"
+
+T = 70
+dt = 0.005
+
+dimension = 1
+ncomponents = 1
+
+# Note: the eps in the paper is our eps**2
+eps = 0.1530417681822
+
+potential = "eckart"
+sigma = 100 * 3.8008 * 10**(-4.0)
+a =  1.0 / (2.0 * 0.52918)
+
+# The parameter set of the initial wavepacket
+Q = [[ 3.5355339059327 ]]
+P = [[ 0.2828427124746j]]
+q = [[-7.5589045088306 ]]
+p = [[ 0.2478854736792 ]]
+S = [[0.0]]
+
+# What it takes to specify a wavepacket!
+wp0 = {
+    "type" : "HagedornWavepacket",
+    "dimension" : dimension,
+    "ncomponents": ncomponents,
+    "eps" : eps,
+    "Pi" : [q,p,Q,P,S],
+    "basis_shapes" : [{
+        "type" : "HyperbolicCutShape",
+        "K" : 512,
+        "dimension" : 1
+    }],
+    "coefficients" : [[ ((0,), 1.0) ]],
+    "innerproduct" : {
+        "type" : "HomogeneousInnerProduct",
+        "delegate" : {
+            "type" : "DirectHomogeneousQuadrature",
+            'qr': {
+                'type': 'TensorProductQR',
+                'dimension': 1,
+                'qr_rules': [{'dimension': dimension, 'order': 516, 'type': 'GaussHermiteQR'}]
+            }
+        }
+    }
+}
+
+# Which wavepackets are initial values
+initvals = [ wp0 ]
+
+leading_component = 0
+
+# How often do we write data to disk
+write_nth = 20
+
+matrix_exponential = "arnoldi"
+arnoldi_steps = 15
