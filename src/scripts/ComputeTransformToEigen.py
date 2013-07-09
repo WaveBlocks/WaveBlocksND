4cab6bbb3651250be6cd26459d687ade0572c82a
Raoul Bourquin <raoulb@student.ethz.ch>
2013-07-09 15:35:34 +0200

    Implement argparse for remaining computation scripts

--- src/scripts/ComputeTransformToEigen.py
+++ src/scripts/ComputeTransformToEigen.py
@@ -7,61 +7,74 @@ Compute the eigen transformation of some simulation results.
 @license: Modified BSD License
 """
 
-import sys
+import argparse
 
 from WaveBlocksND import IOManager
 from WaveBlocksND import GlobalDefaults as GD
 
+parser = argparse.ArgumentParser()
 
-if __name__ == "__main__":
+parser.add_argument("-i", "--inputfile",
+                    type = str,
+                    help = "The data file to read the data from.",
+                    default = GD.file_resultdatafile)
 
-    iomc = IOManager()
-    iome = IOManager()
+parser.add_argument("-o", "--outputfile",
+                    type = str,
+                    help = "The data file to write the transformed data.")
 
-    # Read file with simulation data
-    try:
-        filename = sys.argv[1]
-    except IndexError:
-        filename = GD.file_resultdatafile
+args = parser.parse_args()
 
-    iomc.open_file(filename=filename)
+if not args.outputfile:
+    outputfile = args.inputfile.replace(GD.ext_resultdatafile, "") + "_eigen" + GD.ext_resultdatafile
+else:
+    outputfile = args.outputfile
 
-    # New file for eigen transformed data
-    P = iomc.load_parameters()
-    iome.create_file(filename[:-5]+"_eigen.hdf5")
+print("Reading simulation data from the file: "+str(args.inputfile))
+print("Writing transformed data to the file: "+str(outputfile))
 
-    # Save the simulation parameters
-    iome.add_parameters()
-    iome.save_parameters(P)
+# Read file with simulation data
+iomc = IOManager()
+iomc.open_file(args.inputfile)
 
-    # Iterate over all groups
-    for groupid in iomc.get_group_ids():
+iome = IOManager()
+iome.create_file(outputfile)
 
-        # Create the group if necessary
-        if not groupid in iome.get_group_ids():
-            iome.create_group(groupid=groupid)
+# New file for eigen transformed data
+P = iomc.load_parameters()
 
-        for blockid in iomc.get_block_ids(groupid=groupid):
-            print("Computing eigentransformation of data in block '"+str(blockid)+"'")
+# Save the simulation parameters
+iome.add_parameters()
+iome.save_parameters(P)
 
-            # Create the block if necessary
-            if not blockid in iome.get_block_ids(groupid=groupid):
-                iome.create_block(blockid=blockid, groupid=groupid)
+# Iterate over all groups
+for groupid in iomc.get_group_ids():
 
-            # See if we have a wavefunction
-            if iomc.has_wavefunction(blockid=blockid):
-                from EigentransformWavefunction import transform_wavefunction_to_eigen
-                transform_wavefunction_to_eigen(iomc, iome, blockidin=blockid, blockidout=blockid)
+    # Create the group if necessary
+    if not groupid in iome.get_group_ids():
+        iome.create_group(groupid=groupid)
 
-            # See if we have a homogeneous wavepacket next
-            if iomc.has_wavepacket(blockid=blockid):
-                from EigentransformHagedornWavepacket import transform_hawp_to_eigen
-                transform_hawp_to_eigen(iomc, iome, blockidin=blockid, blockidout=blockid)
+    for blockid in iomc.get_block_ids(groupid=groupid):
+        print("Computing eigentransformation of data in block '"+str(blockid)+"'")
 
-            # See if we have an inhomogeneous wavepacket next
-            if iomc.has_inhomogwavepacket(blockid=blockid):
-                from EigentransformHagedornWavepacket import transform_inhawp_to_eigen
-                transform_inhawp_to_eigen(iomc, iome, blockidin=blockid, blockidout=blockid)
+        # Create the block if necessary
+        if not blockid in iome.get_block_ids(groupid=groupid):
+            iome.create_block(blockid=blockid, groupid=groupid)
 
-    iomc.finalize()
-    iome.finalize()
+        # See if we have a wavefunction
+        if iomc.has_wavefunction(blockid=blockid):
+            from EigentransformWavefunction import transform_wavefunction_to_eigen
+            transform_wavefunction_to_eigen(iomc, iome, blockidin=blockid, blockidout=blockid)
+
+        # See if we have a homogeneous wavepacket next
+        if iomc.has_wavepacket(blockid=blockid):
+            from EigentransformHagedornWavepacket import transform_hawp_to_eigen
+            transform_hawp_to_eigen(iomc, iome, blockidin=blockid, blockidout=blockid)
+
+        # See if we have an inhomogeneous wavepacket next
+        if iomc.has_inhomogwavepacket(blockid=blockid):
+            from EigentransformHagedornWavepacket import transform_inhawp_to_eigen
+            transform_inhawp_to_eigen(iomc, iome, blockidin=blockid, blockidout=blockid)
+
+iomc.finalize()
+iome.finalize()
