fc620912e09f9aa17915e7f6e08ef89393f76df9
Raoul Bourquin <raoulb@student.ethz.ch>
2013-09-06 12:58:40 +0200

    Add more information to new potential

--- src/WaveBlocksND/PotentialLibrary.py
+++ src/WaveBlocksND/PotentialLibrary.py
@@ -188,6 +188,7 @@ eckart_bn = {}
 eckart_bn["variables"] = ["x", "y"]
 eckart_bn["potential"] = "v0*cosh(a*x)**(-2) + k/2*(1-sigma*exp(-lam*x**2)) * y**2"
 eckart_bn["defaults"] = {"v0":"0.425", "a":"1.3624", "k":"0.06784", "sigma":"0.5", "lam":"0.25"}
+eckart_bn["number_levels"] = 1
 
 #######################################################################
 # Potentials in three dimensions and with one energy level (D=3, N=1) #
