65c23ad05d43b5bcb8c24f509f6b3aa5159406ad
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2014-09-04 18:23:57 +0200

    Clean up processing splitting methods

--- src/WaveBlocksND/ProcessingSplittingParameters.py
+++ src/WaveBlocksND/ProcessingSplittingParameters.py
@@ -27,35 +27,38 @@ class ProcessingSplittingParameters(object):
         BCR764 (7,6,4) Blanes/Casas/Ros  [1]_ table (iv)
         ====== ======= ================= =========
 
-        .. [1] Blanes, Casas, Ros 2000, table IV
+        .. [1] S. Blanes, F. Casas, and J. Ros,
+               "Symplectic Integration with Processing: A General Study",
+               SIAM Journal on Scientific Computing, Volume 21, Issue 2, (1999) 711-727.
         """
         if method == "BCR764":
             # Kernel Pattern ABA
             # Exchanged a and b compared to the paper in
             # order to have consistency with other code
             a = zeros(4)
-            a[1] = 1.5171479707207228
-            a[2] = 1.0 - 2.0 * a[1]
-            a[3] = 1.0 * a[1]
+            a[0] =  0.0
+            a[1] =  1.5171479707207228
+            a[2] = -2.0342959414414454
+            a[3] =  1.5171479707207228
             b = zeros(4)
-            b[0] = 0.5600879810924619
-            b[1] = 0.5 - b[0]
-            b[2] = 1.0 * b[1]
-            b[3] = 1.0 * b[0]
+            b[0] =  0.5600879810924619
+            b[1] = -0.06008798109246194
+            b[2] = -0.06008798109246194
+            b[3] =  0.5600879810924619
             # Pre/Post-Processor
             z = zeros(6)
             z[0] = -0.3346222298730
-            z[1] = 1.097567990732164
+            z[1] =  1.097567990732164
             z[2] = -1.038088746096783
-            z[3] = 0.6234776317921379
+            z[3] =  0.6234776317921379
             z[4] = -1.102753206303191
             z[5] = -0.0141183222088869
             y = zeros(6)
             y[0] = -1.621810118086801
-            y[1] = 0.0061709468110142
-            y[2] = 0.8348493592472594
+            y[1] =  0.0061709468110142
+            y[2] =  0.8348493592472594
             y[3] = -0.0511253369989315
-            y[4] = 0.5633782670698199
+            y[4] =  0.5633782670698199
             y[5] = -0.5
         else:
             raise NotImplementedError("Unknown method: " + method)
