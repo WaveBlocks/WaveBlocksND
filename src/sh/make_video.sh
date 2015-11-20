679e457ac8d16849814fdd7461a41a253917b257
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2015-11-20 16:18:40 +0100

    New video maker

--- src/sh/make_video.sh
+++ src/sh/make_video.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
+# See for example:
+# http://electron.mit.edu/~gsteele/ffmpeg/
 
-echo $1
-
-mencoder mf://./$1*.png  -fps 5 -ofps 5 -o video.flv -of lavf -oac mp3lame -lameopts abr:br=64 -srate 22050 -ovc lavc -lavcopts vcodec=flv:keyint=50:vbitrate=700:mbd=2:mv0:trell:v4mv:cbp:last_pred=3 -vf yadif -sws 9
+ffmpeg -r 20 -pattern_type glob -i "${1}*.png" "${1}.mp4"
