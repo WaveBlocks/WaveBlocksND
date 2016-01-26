d70e7865a519211668e67027961e5b8cd15e37c3
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2016-01-26 18:01:10 +0100

    Basic setup command
    Description:
    https://docs.python.org/3/distutils/apiref.html?highlight=setup#distutils.core.setup

--- setup.py
+++ setup.py
@@ -4,23 +4,29 @@ def readme():
     with open('README.md') as f:
         return f.read()
 
-setup(name='WaveBlocksND',
-      version='0.5',
-      description=u'Reusable building blocks for simulations with semiclassical wavepackets for solving the time-dependent Schrödinger equation.',
-      long_description=readme(),
-      classifiers=[
+setup(name = 'WaveBlocksND',
+      version = '0.5',
+      description = u'Reusable building blocks for simulations with semiclassical wavepackets.',
+      long_description = readme(),
+      author = 'R. Bourquin',
+      author_email = 'raoul.bourquin@sam.math.ethz.ch',
+      url = 'https://github.com/WaveBlocks/WaveBlocksND',
+      packages = ['WaveBlocksND'],
+      scripts = [],
+      classifiers = [
+        'Development Status :: 4 - Beta',
+        'Topic :: Scientific/Engineering :: Mathematics',
+        'Topic :: Scientific/Engineering :: Physics',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Intended Audience :: Science/Research',
         'License :: OSI Approved :: BSD License',
+        'Environment :: Console',
         'Programming Language :: Python :: 2.7',
       ],
-      url='https://github.com/WaveBlocks/WaveBlocksND',
-      author='R. Bourquin',
-      license='BSD',
-      packages=['WaveBlocksND'],
-      install_requires=[
+      license = 'BSD',
+      install_requires = [
         'h5py',
         'sympy',
         'numpy',
         'scipy',
-      ],
-      include_package_data=True,
-      zip_safe=False)
+      ])
