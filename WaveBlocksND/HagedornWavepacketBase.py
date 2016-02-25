Fix formula for new recursion

WaveBlocksND/HagedornWavepacketBase.py
@@ -8,8 +8,9 @@ This file contains the basic interface for general wavepackets.
 """
 
 from numpy import array, atleast_2d, complexfloating, cumsum, dot, einsum, pi, vsplit, vstack, zeros, conjugate, transpose
+from numpy.linalg import eigh
 from scipy import exp, sqrt
-from scipy.linalg import det, inv, norm, eig, polar
+from scipy.linalg import det, inv, norm, polar
 
 from WaveBlocksND.Wavepacket import Wavepacket
 from WaveBlocksND.AbstractGrid import AbstractGrid
@@ -323,11 +324,13 @@ class HagedornWavepacketBase(Wavepacket):
         QQ = dot(Qinv, Qbar)
 
         if new:
-            PA, UA = polar(Q, side='left')
-            _, EV = eig(PA)
-            AL = dot(conjugate(transpose(EV)), UA)
+            UA, PA = polar(Q, side='left')
+            _, EV = eigh(PA)
+            AL = dot(transpose(conjugate(EV)), UA)
+            # ALhinv = transpose(conjugate(inv(AL)))
+            ALhinv = conjugate(inv(AL))
             Qinv = dot(AL, Qinv)
-            QQ = dot(AL, dot(QQ, conjugate(transpose(AL))))
+            QQ = dot(AL, dot(QQ, ALhinv))
 
         # Compute the ground state phi_0 via direct evaluation
         mu0 = bas[tuple(D * [0])]