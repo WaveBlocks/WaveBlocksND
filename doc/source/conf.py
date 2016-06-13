37b1671ec663ea72aca0cf8940630507dcd51716
Raoul Bourquin <raoul.bourquin@sam.math.ethz.ch>
2016-06-13 20:05:16 +0200

    Doc position operator

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'WaveBlocksND'
-copyright = u'2015, R. Bourquin'
+copyright = u'2016, R. Bourquin'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
